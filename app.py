6be22ab248e7f66e42bcf030cb1eece80bd958ef
clark0408 <clark910408@gmail.com>
2022-06-07 22:30:20 +0800

    fuj

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ def handle_message(event):
     msg = event.message.text
     r = "很抱歉你在問什麼?"
     if msg == 'hi':
-        r == 'hi'
+        r = 'hi'
     elif msg == '吃飽沒'
-        r == '還沒'
+        r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='吵喔'))
